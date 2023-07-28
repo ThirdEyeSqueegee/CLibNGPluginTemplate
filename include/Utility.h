6a56b319eb79475cf79701728a11f9aec9451c30
Angad <angadmisra28@gmail.com>
2023-07-28 13:09:05 -0700

    Update template

--- include/Utility.h
+++ include/Utility.h
@@ -6,10 +6,10 @@ protected:
     ~Utility() = default;
 
 public:
-    Utility(const Utility&) = delete; // Prevent copy construction
-    Utility(Utility&&) = delete; // Prevent move construction
+    Utility(const Utility&) = delete;            // Prevent copy construction
+    Utility(Utility&&) = delete;                 // Prevent move construction
     Utility& operator=(const Utility&) = delete; // Prevent copy assignment
-    Utility& operator=(Utility&&) = delete; // Prevent move assignment
+    Utility& operator=(Utility&&) = delete;      // Prevent move assignment
 
     static Utility* GetSingleton() {
         static Utility singleton;
