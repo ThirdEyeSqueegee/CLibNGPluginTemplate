971de1db8c8f4439d26601c693f9ba719311d610
Angad <angadmisra28@gmail.com>
2023-08-17 15:10:17 -0700

    Update template

--- include/Events.h
+++ include/Events.h
@@ -1,5 +1,21 @@
 #pragma once
 
 namespace Events {
+    class OnEquipEventHandler : public RE::BSTEventSink<RE::TESEquipEvent> {
+    protected:
+        OnEquipEventHandler() = default;
+        ~OnEquipEventHandler() override = default;
 
+    public:
+        OnEquipEventHandler(const OnEquipEventHandler&) = delete;
+        OnEquipEventHandler(OnEquipEventHandler&&) = delete;
+        OnEquipEventHandler& operator=(const OnEquipEventHandler&) = delete;
+        OnEquipEventHandler& operator=(OnEquipEventHandler&&) = delete;
+
+        static OnEquipEventHandler* GetSingleton();
+
+        RE::BSEventNotifyControl ProcessEvent(const RE::TESEquipEvent* a_event, RE::BSTEventSource<RE::TESEquipEvent>* a_eventSource) override;
+
+        static void Register();
+    };
 }
