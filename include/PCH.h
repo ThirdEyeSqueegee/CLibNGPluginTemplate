4c2547f679f61c75c773b748a82e2cbee84c9956
Angad <angadmisra28@gmail.com>
2023-08-25 01:57:00 -0700

    Update template

--- include/PCH.h
+++ include/PCH.h
@@ -14,8 +14,8 @@
 #include <cstdarg>
 #include <cstddef>
 #include <cstdint>
-#include <cstdlib>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <ctime>
 #include <cuchar>
@@ -45,8 +45,8 @@
 #include <future>
 #include <initializer_list>
 #include <iomanip>
-#include <iosfwd>
 #include <ios>
+#include <iosfwd>
 #include <iostream>
 #include <istream>
 #include <iterator>
@@ -65,8 +65,8 @@
 #include <queue>
 #include <random>
 #include <ranges>
-#include <regex>
 #include <ratio>
+#include <regex>
 #include <scoped_allocator>
 #include <semaphore>
 #include <set>
@@ -83,9 +83,9 @@
 #include <system_error>
 #include <thread>
 #include <tuple>
+#include <type_traits>
 #include <typeindex>
 #include <typeinfo>
-#include <type_traits>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
@@ -95,36 +95,34 @@
 #include <version>
 
 #include <RE/Skyrim.h>
-#include <SKSE/SKSE.h>
 #include <REL/Relocation.h>
+#include <SKSE/SKSE.h>
 
+// clang-format off
 #include <ShlObj_core.h>
-#include <Windows.h>
 #include <Psapi.h>
-#undef cdecl // Workaround for Clang 14 CMake configure error.
+#include <Windows.h>
+// clang-format on
 
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/msvc_sink.h>
-
-// Compatible declarations with other sample projects.
-#define DLLEXPORT __declspec(dllexport)
 
 using namespace std::literals;
 using namespace REL::literals;
 
 namespace logger = SKSE::log;
 
-template <class T>
+template <typename T>
 class Singleton {
 protected:
-    constexpr Singleton() = default;
+    constexpr Singleton()  = default;
     constexpr ~Singleton() = default;
 
 public:
-    constexpr Singleton(const Singleton&) = delete;
-    constexpr Singleton(Singleton&&) = delete;
+    constexpr Singleton(const Singleton&)      = delete;
+    constexpr Singleton(Singleton&&)           = delete;
     constexpr auto operator=(const Singleton&) = delete;
-    constexpr auto operator=(Singleton&&) = delete;
+    constexpr auto operator=(Singleton&&)      = delete;
 
     static T* GetSingleton() {
         static T singleton;
@@ -132,67 +130,67 @@ public:
     }
 };
 
-template <class T, class E>
-class EventSingleton : public RE::BSTEventSink<E> {
+template <typename TDerived, typename TEvent>
+class EventSingleton : public RE::BSTEventSink<TEvent> {
 protected:
-    constexpr EventSingleton() = default;
+    constexpr EventSingleton()           = default;
     constexpr ~EventSingleton() override = default;
 
 public:
     constexpr EventSingleton(const EventSingleton&) = delete;
-    constexpr EventSingleton(EventSingleton&&) = delete;
+    constexpr EventSingleton(EventSingleton&&)      = delete;
     constexpr auto operator=(const EventSingleton&) = delete;
-    constexpr auto operator=(EventSingleton&&) = delete;
+    constexpr auto operator=(EventSingleton&&)      = delete;
 
-    static T* GetSingleton() {
-        static T singleton;
+    static TDerived* GetSingleton() {
+        static TDerived singleton;
         return std::addressof(singleton);
     }
 
     static void Register() {
-        using eventsource_t = RE::BSTEventSource<E>;
+        using TEventSource = RE::BSTEventSource<TEvent>;
 
-        auto name = std::string(typeid(E).name());
-        const std::regex p("struct |RE::|SKSE::| * __ptr64");
+        auto             name{ std::string(typeid(TEvent).name()) };
+        const std::regex p{ "struct |RE::|SKSE::| * __ptr64" };
         name = std::regex_replace(name, p, "");
 
-        if constexpr (std::is_base_of_v<eventsource_t, RE::BSInputDeviceManager>) {
-            const auto manager = RE::BSInputDeviceManager::GetSingleton();
+        if constexpr (std::is_base_of_v<TEventSource, RE::BSInputDeviceManager>) {
+            const auto manager{ RE::BSInputDeviceManager::GetSingleton() };
             manager->AddEventSink(GetSingleton());
             logger::info("Registered {} handler", name);
             return;
-        } else if constexpr (std::is_base_of_v<eventsource_t, RE::UI>) {
-            const auto ui = RE::UI::GetSingleton();
+        } else if constexpr (std::is_base_of_v<TEventSource, RE::UI>) {
+            const auto ui{ RE::UI::GetSingleton() };
             ui->AddEventSink(GetSingleton());
             logger::info("Registered {} handler", name);
             return;
-        } else if constexpr (std::is_same_v<E, SKSE::ActionEvent>) {
+        } else if constexpr (std::is_same_v<TEvent, SKSE::ActionEvent>) {
             SKSE::GetActionEventSource()->AddEventSink(GetSingleton());
             logger::info("Registered {} handler", name);
             return;
-        } else if constexpr (std::is_same_v<E, SKSE::CameraEvent>) {
+        } else if constexpr (std::is_same_v<TEvent, SKSE::CameraEvent>) {
             SKSE::GetCameraEventSource()->AddEventSink(GetSingleton());
             logger::info("Registered {} handler", name);
             return;
-        } else if constexpr (std::is_same_v<E, SKSE::CrosshairRefEvent>) {
+        } else if constexpr (std::is_same_v<TEvent, SKSE::CrosshairRefEvent>) {
             SKSE::GetCrosshairRefEventSource()->AddEventSink(GetSingleton());
             logger::info("Registered {} handler", name);
             return;
-        } else if constexpr (std::is_same_v<E, SKSE::ModCallbackEvent>) {
+        } else if constexpr (std::is_same_v<TEvent, SKSE::ModCallbackEvent>) {
             SKSE::GetModCallbackEventSource()->AddEventSink(GetSingleton());
             logger::info("Registered {} handler", name);
             return;
-        } else if constexpr (std::is_same_v<E, SKSE::NiNodeUpdateEvent>) {
+        } else if constexpr (std::is_same_v<TEvent, SKSE::NiNodeUpdateEvent>) {
             SKSE::GetNiNodeUpdateEventSource()->AddEventSink(GetSingleton());
             logger::info("Registered {} handler", name);
             return;
-        } else if constexpr (std::is_base_of_v<eventsource_t, RE::ScriptEventSourceHolder>) {
-            const auto holder = RE::ScriptEventSourceHolder::GetSingleton();
+        } else if constexpr (std::is_base_of_v<TEventSource, RE::ScriptEventSourceHolder>) {
+            const auto holder{ RE::ScriptEventSourceHolder::GetSingleton() };
             holder->AddEventSink(GetSingleton());
             logger::info("Registered {} handler", name);
             return;
         }
-        const auto plugin = SKSE::PluginDeclaration::GetSingleton();
+        const auto plugin{ SKSE::PluginDeclaration::GetSingleton() };
         SKSE::stl::report_and_fail(fmt::format("{}: Failed to register {} handler", plugin->GetName(), name));
     }
 };
@@ -200,20 +198,20 @@ public:
 namespace stl {
     using namespace SKSE::stl;
 
-    template <class T>
+    template <typename T>
     void write_thunk_call() {
         SKSE::AllocTrampoline(14);
-        auto& trampoline = SKSE::GetTrampoline();
+        auto& trampoline{ SKSE::GetTrampoline() };
         T::func = trampoline.write_call<5>(T::address, T::Thunk);
     }
 
-    template <class S, class T>
+    template <typename TDest, typename TSource>
     void write_vfunc() {
-        REL::Relocation<std::uintptr_t> vtbl{ S::VTABLE[0] };
-        T::func = vtbl.write_vfunc(T::idx, T::Thunk);
+        REL::Relocation<std::uintptr_t> vtbl{ TDest::VTABLE[0] };
+        TSource::func = vtbl.write_vfunc(TSource::idx, TSource::Thunk);
     }
 
-    template <class T>
+    template <typename T>
     void write_vfunc(const REL::VariantID variant_id) {
         REL::Relocation<std::uintptr_t> vtbl{ variant_id };
         T::func = vtbl.write_vfunc(T::idx, T::Thunk);
