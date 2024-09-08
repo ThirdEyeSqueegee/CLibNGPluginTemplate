7a4ef05cbc6df67f9bc6f09a6970e135d6385d5e
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2024-09-07 23:26:52 -0700

    Update template

--- update_baselines.py
+++ update_baselines.py
@@ -3,6 +3,7 @@ from subprocess import run
 
 vcpkg_repo = "https://github.com/microsoft/vcpkg"
 clib_vcpkg_registry_repo = "https://github.com/ThirdEyeSqueegee/CommonLibSSE-NG-vcpkg"
+custom_vcpkg_registry_repo = "https://github.com/ThirdEyeSqueegee/vcpkg-ports"
 
 vcpkg_ref = (
     run(["git", "ls-remote", vcpkg_repo, "refs/heads/master"], capture_output=True)
@@ -18,11 +19,21 @@ clib_vcpkg_registry_ref = (
     .split()[0]
 )
 
+custom_vcpkg_registry_repo_ref = (
+    run(["git", "ls-remote", custom_vcpkg_registry_repo], capture_output=True)
+    .stdout.decode()
+    .splitlines()[0]
+    .split()[0]
+)
+
 with open("./vcpkg.json", "r") as f:
     vcpkg_json = load(f)
 
 vcpkg_json["builtin-baseline"] = vcpkg_ref
 vcpkg_json["vcpkg-configuration"]["registries"][0]["baseline"] = clib_vcpkg_registry_ref
+vcpkg_json["vcpkg-configuration"]["registries"][1]["baseline"] = (
+    custom_vcpkg_registry_repo_ref
+)
 
 with open("./vcpkg.json", "w") as f:
     dump(vcpkg_json, f, indent=2)
