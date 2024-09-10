86bf0a5bbe78f279674bff6579677cfd441fd73f
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2024-09-09 18:47:14 -0700

    Update template

--- project_setup.py
+++ project_setup.py
@@ -1,24 +1,24 @@
-import os
-import subprocess
-import shutil
+from os import access, chmod, remove, rename, environ, W_OK
+from os.path import isdir, join
+from shutil import rmtree
+from subprocess import run
 import stat
-from update_baselines import update_baselines
 
 
 def onexc(func, path, exc_info):
-    if not os.access(path, os.W_OK):
-        os.chmod(path, stat.S_IWUSR)
+    if not access(path, W_OK):
+        chmod(path, stat.S_IWUSR)
         func(path)
     else:
         raise
 
 
 # Remove original git repo
-if os.path.isdir(".git"):
-    shutil.rmtree(".git", onexc=onexc)
+if isdir(".git"):
+    rmtree(".git", onexc=onexc)
 
 # Remove README.md
-os.remove("README.md")
+remove("README.md")
 
 # Get project name and author
 project_name = input("Enter project name: ")
@@ -26,42 +26,38 @@ author = input("Enter author: ")
 print()
 
 # Update CMakeLists.txt
-with open("CMakeLists.txt", "r", encoding="utf-8") as cmakelists_file:
-    cmakelists = cmakelists_file.read()
+with open("CMakeLists.txt", "r", encoding="utf-8") as f:
+    cmakelists = f.read()
 
 cmakelists = cmakelists.replace("PluginName", project_name)
 cmakelists = cmakelists.replace("AuthorName", author)
 cmakelists = cmakelists.replace("0.0.1", "1.0.0")
 
-with open("CMakeLists.txt", "w", encoding="utf-8") as cmakelists_file:
-    cmakelists_file.write(cmakelists)
+with open("CMakeLists.txt", "w", encoding="utf-8") as f:
+    f.write(cmakelists)
 
 # Rename ini file
-os.rename(
-    os.path.join("contrib", "Config", "PluginName.ini"),
-    os.path.join("contrib", "Config", f"{project_name}.ini"),
+rename(
+    join("contrib", "Config", "PluginName.ini"),
+    join("contrib", "Config", f"{project_name}.ini"),
 )
 
 # Update Settings.cpp
-with open(
-    os.path.join("src", "Settings.cpp"), "r", encoding="utf-8"
-) as settings_cpp_file:
-    settings_cpp = settings_cpp_file.read()
+with open(join("src", "Settings.cpp"), "r", encoding="utf-8") as f:
+    settings_cpp = f.read()
 
 settings_cpp = settings_cpp.replace("PluginName.ini", f"{project_name}.ini")
 
-with open(
-    os.path.join("src", "Settings.cpp"), "w", encoding="utf-8"
-) as settings_cpp_file:
-    settings_cpp_file.write(settings_cpp)
+with open(join("src", "Settings.cpp"), "w", encoding="utf-8") as f:
+    f.write(settings_cpp)
 
 # Update baselines
 print("Updating vcpkg.json baselines...")
-update_baselines()
+run([f"{environ["VCPKG_ROOT"]}/vcpkg.exe", "x-update-baseline"])
 print()
 
 # Initialize empty git repo
-subprocess.run(["git", "init"])
+run(["git", "init"])
 
 # Self-destruct
-os.remove(__file__)
+remove(__file__)
