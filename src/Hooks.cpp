b27e12a31e5d740fdde581fee86281419123e5c1
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2024-11-03 20:32:05 -0800

    Update template

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -4,7 +4,7 @@ namespace Hooks
 {
     void Install() noexcept
     {
-        static const REL::Relocation main_update{ RELOCATION_ID(35565, 36564), REL::Relocate(0x748, 0xc26, 0x7ee) };
+        const REL::Relocation main_update{ RELOCATION_ID(35565, 36564), REL::Relocate(0x748, 0xc26, 0x7ee) };
         stl::write_thunk_call<MainUpdate>(main_update.address());
         logger::info("Installed main update hook");
         logger::info("");
